e9c6c5da186fba83ba08a7cd7fcea438e1ce92ed
Thomas Mota <tmm@danskecommodities.com>
2022-10-17 13:08:21 +0200

    change name

--- apis/v1alpha1/groupversion_info.go
+++ apis/v1alpha1/groupversion_info.go
@@ -14,9 +14,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package v1alpha1 contains the core resources of the Template provider.
+// Package v1alpha1 contains the core resources of the BitbucketServer provider.
 // +kubebuilder:object:generate=true
-// +groupName=template.crossplane.io
+// +groupName=bitbucketserver.crossplane.io
 // +versionName=v1alpha1
 package v1alpha1
 
@@ -27,7 +27,7 @@ import (
 
 // Package type metadata.
 const (
-	Group   = "template.crossplane.io"
+	Group   = "bitbucketserver.crossplane.io"
 	Version = "v1alpha1"
 )
 
--- internal/controller/bitbucketserver.go
+++ internal/controller/bitbucketserver.go
@@ -20,16 +20,14 @@ import (
 	"github.com/crossplane/crossplane-runtime/pkg/controller"
 	ctrl "sigs.k8s.io/controller-runtime"
 
-	"github.com/crossplane/provider-template/internal/controller/config"
-	"github.com/crossplane/provider-template/internal/controller/mytype"
+	"github.com/tomas-mota/provider-bitbucketserver/internal/controller/config"
 )
 
-// Setup creates all Template controllers with the supplied logger and adds them to
+// Setup creates all BitbucketServer controllers with the supplied logger and adds them to
 // the supplied manager.
 func Setup(mgr ctrl.Manager, o controller.Options) error {
 	for _, setup := range []func(ctrl.Manager, controller.Options) error{
 		config.Setup,
-		mytype.Setup,
 	} {
 		if err := setup(mgr, o); err != nil {
 			return err
